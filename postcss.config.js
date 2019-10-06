348d6493387d7aa7a1fbb79ba339dab05642b37b
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-10-07 00:10:46 +0100

    :wrench: Update postcss to whitelist all colours.

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,15 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+
+  // Purgecss doesn't know how to look for things like computed classes. These
+  // are mostly used background and text colours, so we're going to whitelist all
+  // colours. This will bloat the bundle slightly, but it's more than worth the
+  // tradeoff.
+  whitelistPatterns: [
+    /[1-9]00/
+  ]
 })
 
 module.exports = {
