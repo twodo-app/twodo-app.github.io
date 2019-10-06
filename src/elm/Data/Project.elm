16c73f35b75ce9b925e5a81fb959b4715fb5d6d6
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-10-06 18:42:47 +0100

    :truck: Rename Day.elm -> Project.elm

--- src/elm/Data/Project.elm
+++ src/elm/Data/Project.elm
@@ -1,9 +1,10 @@
-module Data.Day exposing
+module Data.Project exposing
   ( ..
   )
 
 -- Imports ---------------------------------------------------------------------
-import Html exposing (Html)
+import Data.Task exposing (Task)
+import Html exposing (Html, Attribute)
 import Html.Attributes
 import Html.Events
 import Json.Decode as Decode
@@ -11,7 +12,7 @@ import Json.Encode as Encode exposing (Value)
 
 -- Types -----------------------------------------------------------------------
 type alias Day =
-  {
+  { tasks : List Task
   }
 
 -- Functions -------------------------------------------------------------------
@@ -20,7 +21,12 @@ type alias Day =
 -- View ------------------------------------------------------------------------
 view : Day -> Html msg
 view day =
-  Html.div []
+  let
+    classes : Attribute msg
+    classes =
+      Html.Attributes.class "p-4 m-4 rounded-lg bg-gray-200"
+  in
+  Html.div [ classes ]
     []
 
 -- Json ------------------------------------------------------------------------
